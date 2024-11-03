436594b3d5c67650062eaff6375769ea49e92cac
chester <chestersykes1@gmail.com>
2024-11-03 16:04:50 +0800

    changed to use the title as the slug

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -17,10 +18,11 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = $this->faker->sentence;
         return [
             'user_id' => User::factory()->create()->id,
-            'title' => $this->faker->sentence,
-            'slug' => $this->faker->slug,
+            'title' => $title,
+            'slug' => Str::slug($title),
             'content' => $this->faker->paragraph,
         ];
     }
