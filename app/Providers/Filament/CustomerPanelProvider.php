66638cf67290f7f942493fe55b379f9c1e6f599f
chester <chestersykes1@gmail.com>
2024-11-03 16:02:06 +0800

    added login

--- app/Providers/Filament/CustomerPanelProvider.php
+++ app/Providers/Filament/CustomerPanelProvider.php
@@ -28,6 +28,7 @@ class CustomerPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->login()
             ->discoverResources(in: app_path('Filament/Customer/Resources'), for: 'App\\Filament\\Customer\\Resources')
             ->discoverPages(in: app_path('Filament/Customer/Pages'), for: 'App\\Filament\\Customer\\Pages')
             ->pages([
